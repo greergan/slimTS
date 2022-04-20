d280cab608e1374bf460dcb3edb62501e463551a
Jeff Greer <greergan@gmail.com>
2022-04-19 17:59:18 -0700

    removed unused header

--- program/include/http_server.hpp
+++ program/include/http_server.hpp
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <string>
 #include <logger.hpp>
-#include <v8pp/class.hpp>
 #include <v8pp/module.hpp>
 std::string ValueToString(v8::Isolate* isolate, v8::Local<v8::Value> value) {
     v8::String::Utf8Value utf8_value(isolate, value);
