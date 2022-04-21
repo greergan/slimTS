17491dfde0b2dd52f1cb1f2ee78b3bb3d129ae4f
Jeff Greer <greergan@gmail.com>
2022-04-20 22:10:38 -0700

    continuing work on console methods

--- program/include/console.hpp
+++ program/include/console.hpp
@@ -1,3 +1,5 @@
+#ifndef __SLIM__CONSOLE__HPP
+#define __SLIM__CONSOLE_HPP
 #include <any>
 #include <mutex>
 #include <unordered_map>
@@ -8,18 +10,35 @@
 /*
  * Reference https://console.spec.whatwg.org/#printer
  */
-namespace slim::console::configuration {
-    int precision = 10;
-    struct Config {
+namespace slim::console::configuration_templates {
+    struct Configuration {
         bool dim = false;
         bool bold = false;
         bool italic = false;
         bool underline = false;
-        std::string text = "default";
-        std::string background = "default";
+        std::string text_color = "default";
+        std::string background_color = "default";
     };
-    Config log{};
-    Config dir{};
+    struct ExtendedConfiguration: Configuration {
+        std::string level_string;
+        Configuration log_level{};
+        Configuration location{};
+        struct message {
+            Configuration text{};
+            Configuration value{};
+        };
+        Configuration remainder{};
+        ExtendedConfiguration(const std::string level, const std::string color) : level_string{level} {
+            text_color = color;
+        }
+    };
+}
+namespace slim::console::configuration {
+    int precision = 10;
+
+    slim::console::configuration_templates::Configuration dir{};
+    slim::console::configuration_templates::Configuration log{};
+    slim::console::configuration_templates::ExtendedConfiguration todo("TODO", "blue");
 }
 namespace slim::console::colors {
     std::unordered_map<std::string, int> text {
@@ -62,10 +81,6 @@ namespace slim::console::colors {
     };
 }
 namespace slim::console {
-    std::string ValueToString(v8::Isolate* isolate, v8::Local<v8::Value> value) {
-        v8::String::Utf8Value utf8_value(isolate, value);
-        return std::string(*utf8_value);
-    }
     void clear() {
         std::cerr << "\x1B[2J\x1B[H";
     }
@@ -73,26 +88,20 @@ namespace slim::console {
         if(!args[0]->IsObject()) { return; }
         v8::Isolate* isolate = args.GetIsolate();
         v8::HandleScope scope(isolate);
-        v8::Local<v8::Context> context = isolate->GetCurrentContext();
-        v8::Local<v8::Object> configurations = args[0]->ToObject(context).ToLocalChecked();
-        v8::Local<v8::Name> log_name = v8::String::NewFromUtf8(isolate, "log").ToLocalChecked();
-        v8::Local<v8::Value> log_configuration = configurations->Get(context, log_name).ToLocalChecked();
-        if(log_configuration->IsObject()) {
-            v8::Local<v8::Object> config = log_configuration->ToObject(context).ToLocalChecked();
-            v8::Local<v8::Name> text_name = v8::String::NewFromUtf8(isolate, "text").ToLocalChecked();
-            std::string text = slim::utilies::ValueToString(isolate, config->Get(context, text_name).ToLocalChecked());           
-            if(text != "undefined") {
-                slim::console::configuration::log.text = text;
+        v8::Local<v8::Object> configurations = slim::utilities::GetObject(isolate, args[0]);
+        v8::Local<v8::Object> log_configuration = slim::utilities::GetObject(isolate, "log", configurations);
+        if(log_configuration->IsObject() && slim::utilities::PropertyCount(isolate, log_configuration) > 0) {
+            std::string text_color = slim::utilities::StringValue(isolate, "text", log_configuration);
+            if(text_color != "undefined") {
+                slim::console::configuration::log.text_color = text_color;
             }
-            v8::Local<v8::Name> background_name = v8::String::NewFromUtf8(isolate, "background").ToLocalChecked();
-            std::string background = slim::utilies::ValueToString(isolate, config->Get(context, background_name).ToLocalChecked());
-            if(background != "undefined") {
-                slim::console::configuration::log.background = background;
+            std::string background_color = slim::utilities::StringValue(isolate, "background", log_configuration);
+            if(background_color != "undefined") {
+                slim::console::configuration::log.background_color = background_color;
             }
-            v8::Local<v8::Name> dim_name = v8::String::NewFromUtf8(isolate, "port").ToLocalChecked();
-            v8::Local<v8::Value> dim_value = config->Get(context, dim_name).ToLocalChecked();
-            if(dim_value->IsBoolean()) {
-                slim::console::configuration::log.dim = dim_value->BooleanValue(isolate);
+            v8::Local<v8::Value> dim = slim::utilities::GetValue(isolate, "dim", log_configuration);
+            if(dim->IsBoolean()) {
+                slim::console::configuration::log.dim = dim->BooleanValue(isolate);
             }
         }
     }
@@ -103,9 +112,10 @@ namespace slim::console {
         std::string dim;
         std::string italic;
         std::string underline;
-        out(struct slim::console::configuration::Config &config) {
-            text = slim::console::colors::text[config.text];
-            background = slim::console::colors::background[config.background];
+        template<typename T>
+        out(T &config) {
+            text = slim::console::colors::text[config.text_color];
+            background = slim::console::colors::background[config.background_color];
             bold = (config.bold) ? "1;" : "";
             dim = (config.dim) ? "2;" : "";
             italic = (config.italic) ? "3;" : "";
@@ -122,22 +132,22 @@ namespace slim::console {
         }
     };
     void log(const FunctionCallbackInfo<Value>& args) {
+        if(args.Length() == 0) { return; }
         v8::Isolate* isolate = args.GetIsolate();
         v8::HandleScope scope(isolate);
-        v8::Local<v8::Context> context = isolate->GetCurrentContext();
         std::mutex mtx;
         out print = out(slim::console::configuration::log);
         mtx.lock();
         std::cerr.precision(slim::console::configuration::precision);
         for(int i = 0; i < args.Length(); i++) {
             if(args[i]->IsNumber()) {
-                print << args[i]->NumberValue(context).FromJust();
+                print << slim::utilities::NumberValue(isolate, args[i]);
             }
             else if(args[i]->IsString()) {
-                print << ValueToString(isolate, args[i]->ToString(context).ToLocalChecked());
+                print << slim::utilities::StringValue(isolate, args[i]);
             }
             else if(args[i]->IsFunction()) {
-                print << "Function " << ValueToString(isolate, args[i]->ToString(context).ToLocalChecked());
+                print << "Function " << slim::utilities::StringFunction(isolate, args[i]);
             }
             else if(args[i]->IsArray()) {
                 print << "Array " << v8pp::json_str(isolate, args[i]);
@@ -146,7 +156,7 @@ namespace slim::console {
                 print << "Object " << v8pp::json_str(isolate, args[i]);
             }
             else{
-                print << "Typeof " << ValueToString(isolate, args[i]->TypeOf(isolate));
+                print << "Typeof " << slim::utilities::StringValue(isolate, args[i]->TypeOf(isolate));
             }
             if(i != args.Length() - 1) {
                 std::cerr << " ";
@@ -155,11 +165,21 @@ namespace slim::console {
         std::cerr << "\n";
         mtx.unlock();
     }
-    void expose(v8::Isolate* isolate, v8::Local<v8::Context> context) {
+    void todo(const FunctionCallbackInfo<Value>& args) {
+        if(args.Length() == 0) { return; }
+        if(args[0]->IsString()) {
+            v8::Isolate* isolate = args.GetIsolate();
+            v8::HandleScope scope(isolate);
+            out print = out(slim::console::configuration::todo);
+            print << slim::utilities::StringValue(isolate, args[0]);
+        }
+    }
+    void expose(v8::Isolate* isolate) {
         v8pp::module console_module(isolate);
         console_module.set("configure", &configure);
         console_module.set("clear", &clear);
         console_module.set("log", &log);
-        v8::Maybe result = context->Global()->Set(isolate->GetCurrentContext(), v8pp::to_v8(isolate, "console"), console_module.new_instance());
+        isolate->GetCurrentContext()->Global()->Set(isolate->GetCurrentContext(), v8pp::to_v8(isolate, "console"), console_module.new_instance()).ToChecked();
     }
 }
+#endif
--- program/main.cpp
+++ program/main.cpp
@@ -7,7 +7,7 @@
 #include <console.hpp>
 void on_uv_walk(uv_handle_t* handle, void* arg) { uv_close(handle, NULL); }
 void on_sigint_received(uv_signal_t *handle, int signum) {
-    slim::log::system::notice("Slim server shutting down");
+    slim::log::notice("Slim server shutting down");
     int result = uv_loop_close(handle->loop);
     if(result == UV_EBUSY) {
         uv_walk(handle->loop, on_uv_walk, NULL);
@@ -19,8 +19,8 @@ int main(int argc, char *argv[]) {
     uv_signal_start(sigint, on_sigint_received, SIGINT);
     static uv_loop_t* main_loop = uv_default_loop();
     slim::http::init(main_loop);
-    slim::log::system::init(main_loop);
-    slim::log::system::notice("Slim server starting");
+    slim::log::init(main_loop);
+    slim::log::notice("Slim server starting");
     slim::veight::Process process(argc, argv);
     Isolate* isolate = process.GetIsolate();
     Isolate::Scope isolate_scope(isolate);
@@ -28,25 +28,23 @@ int main(int argc, char *argv[]) {
     process.CreateGlobal();
     Local<Context> context = process.GetNewContext(isolate);
     if(context.IsEmpty()) {
-        slim::log::system::critical("Error creating context");
+        slim::log::critical("Error creating context");
         exit(1);
     }
     else {
-        slim::log::system::info("Slim server initialized");
+        slim::log::info("Slim server initialized");
         v8::Isolate::Scope isolate_scope(isolate);
         HandleScope handle_scope(isolate);
         Context::Scope context_scope(context);
-
-        slim::http::expose(isolate, context);
-        slim::console::expose(isolate, context);
-        slim::log::system::expose(isolate, context);       
-        
-        
+        slim::http::expose(isolate);
+        slim::console::expose(isolate);
+        slim::log::expose(isolate);
         Local<String> source = String::NewFromUtf8Literal(isolate, 
         R"(
+            const http_stream = slim.http();
             //const http_stream = slim.http({"port": 8080, "host": "0.0.0.0"});
             console.clear();
-            console.configure({"log": {"text": "red", "background": "default", "dim": true}});
+            console.configure({"log": {"text": "red", "background": "default", "dim": true, "underline": true}});
             console.log("working it out");
             
 /*             for await (const request of http_stream) {
@@ -56,7 +54,7 @@ int main(int argc, char *argv[]) {
         Local<String> name = String::NewFromUtf8Literal(isolate, "test");
         bool success = process.RunScript(isolate, source, name);
     }
-    slim::log::system::handle_libuv_error("slim::main::loop error: ", uv_run(main_loop, UV_RUN_DEFAULT));
+    slim::log::handle_libuv_error("slim::main::loop error: ", uv_run(main_loop, UV_RUN_DEFAULT));
     uv_loop_close(uv_default_loop());
     slim::http::stop();
     delete sigint;
