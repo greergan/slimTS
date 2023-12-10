28e27afdff78239de0396123c9bbe3886b78450d
Jeff Greer <greergan@gmail.com>
2023-12-09 22:52:31 -0800

    added test directory and console.js

---
+++ test/console.js
@@ -0,0 +1,11 @@
+//console.clear()
+console.debug("debug works");
+console.dir("dir works");
+console.dirxml("dirxml works");
+console.error("error works");
+console.info("info works");
+console.log("log works");
+console.print("print works");
+console.warn("warn works");
+console.table("table works");
+console.trace("trace works");
