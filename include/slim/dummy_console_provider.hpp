dcdc5233eec05a21e27c1112a72edb8baf06e881
Jeff Greer <greergan@gmail.com>
2023-12-15 17:57:15 -0800

    refactored away from class in favor of static

--- include/slim/dummy_console_provider.hpp
+++ include/slim/dummy_console_provider.hpp
@@ -2,41 +2,38 @@
 #define __SLIM_DUMMY_CONSOLE_PROVIDER__HPP
 #include <v8.h>
 #include <slim/plugin.hpp>
-class DummyConsoleProvider {
-	static void noContent(const v8::FunctionCallbackInfo<v8::Value>& args);
-public:
-	DummyConsoleProvider();
-	~DummyConsoleProvider();
+/*
+ * Reference https://console.spec.whatwg.org/#printer
+ */
+namespace slim::dummy_console {
 	void expose_plugin(v8::Isolate* isolate);
-};
-DummyConsoleProvider::DummyConsoleProvider(){}
-DummyConsoleProvider::~DummyConsoleProvider(){}
-void DummyConsoleProvider::noContent(const v8::FunctionCallbackInfo<v8::Value>& args){}
-void DummyConsoleProvider::expose_plugin(v8::Isolate* isolate) {
+}
+void slim::dummy_console::expose_plugin(v8::Isolate* isolate) {
+	auto no_content = [](const v8::FunctionCallbackInfo<v8::Value>& args){};
 	slim::plugin::plugin dummy_console_plugin(isolate, "console");
-	dummy_console_plugin.add_function("count", &this->noContent);
- 	dummy_console_plugin.add_function("countReset", &this->noContent);
-	dummy_console_plugin.add_function("group", &this->noContent);
-	dummy_console_plugin.add_function("groupCollapsed", &this->noContent);
-	dummy_console_plugin.add_function("groupEnd", &this->noContent);
-	dummy_console_plugin.add_function("time", &this->noContent);
-	dummy_console_plugin.add_function("timeLog", &this->noContent);
-	dummy_console_plugin.add_function("timeEnd", &this->noContent);
-	dummy_console_plugin.add_function("assert", &this->noContent);
-	dummy_console_plugin.add_function("clear", &this->noContent);
-	dummy_console_plugin.add_function("debug", &this->noContent);
-	dummy_console_plugin.add_function("dir", &this->noContent);
-	dummy_console_plugin.add_function("dirxml", &this->noContent);
-	dummy_console_plugin.add_function("error", &this->noContent);
-	dummy_console_plugin.add_function("info", &this->noContent);
-	dummy_console_plugin.add_function("listen", &this->noContent);
-	dummy_console_plugin.add_function("log", &this->noContent);
-	dummy_console_plugin.add_function("print", &this->noContent);
-	dummy_console_plugin.add_function("table", &this->noContent);
-	dummy_console_plugin.add_function("todo", &this->noContent);
-	dummy_console_plugin.add_function("trace", &this->noContent);
-	dummy_console_plugin.add_function("warn", &this->noContent);
-	dummy_console_plugin.add_function("write", &this->noContent);
+	dummy_console_plugin.add_function("assert",         no_content);
+	dummy_console_plugin.add_function("clear",          no_content);
+	dummy_console_plugin.add_function("count",          no_content);
+ 	dummy_console_plugin.add_function("countReset",     no_content);
+	dummy_console_plugin.add_function("debug",          no_content);
+	dummy_console_plugin.add_function("dir",            no_content);
+	dummy_console_plugin.add_function("dirxml",         no_content);
+	dummy_console_plugin.add_function("error",          no_content);
+	dummy_console_plugin.add_function("group",          no_content);
+	dummy_console_plugin.add_function("groupCollapsed", no_content);
+	dummy_console_plugin.add_function("groupEnd",       no_content);
+	dummy_console_plugin.add_function("info",           no_content);
+	dummy_console_plugin.add_function("listen",         no_content);
+	dummy_console_plugin.add_function("log",            no_content);
+	dummy_console_plugin.add_function("print",          no_content);
+	dummy_console_plugin.add_function("table",          no_content);
+	dummy_console_plugin.add_function("time",           no_content);
+	dummy_console_plugin.add_function("timeEnd",        no_content);
+	dummy_console_plugin.add_function("timeLog",        no_content);
+	dummy_console_plugin.add_function("todo",           no_content);
+	dummy_console_plugin.add_function("trace",          no_content);
+	dummy_console_plugin.add_function("warn",           no_content);
+	dummy_console_plugin.add_function("write",          no_content);
 	dummy_console_plugin.expose_plugin();
 	return;
 }
