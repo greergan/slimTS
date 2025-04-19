09f8ede0d664f670e266aa1639e7f44320047932
Jeff Greer <greergan@gmail.com>
2025-04-19 16:31:30 -0700

    added browser, with exit and nextTick functions

--- src/plugins/process/process.cpp
+++ src/plugins/process/process.cpp
@@ -3,10 +3,29 @@
 #include <slim/plugin.hpp>
 #include <slim/utilities.h>
 namespace slim::process {
-    v8::Isolate* process_isolate = slim::gv8::GetIsolate();
+	bool browser;
+	void exit_wrapper(const v8::FunctionCallbackInfo<v8::Value>& args);
+	void nextTick(const v8::FunctionCallbackInfo<v8::Value>& args);
+}
+void slim::process::exit_wrapper(const v8::FunctionCallbackInfo<v8::Value>& args) {
+	exit(1);
+}
+void slim::process::nextTick(const v8::FunctionCallbackInfo<v8::Value>& args) {
+
 }
 extern "C" void expose_plugin(v8::Isolate* isolate) {
 	slim::plugin::plugin process_plugin(isolate, "process");
+	process_plugin.add_property("browser", &slim::process::browser);
+	process_plugin.add_function("exit", slim::process::exit_wrapper);
+	process_plugin.add_function("nextTick", slim::process::nextTick);
 	process_plugin.expose_plugin();
 	return;
 }
+
+/*
+
+process !== "undefined" && !!process.nextTick && !process.browser
+
+https://nodejs.org/en/learn/asynchronous-work/understanding-processnexttick
+
+*/
