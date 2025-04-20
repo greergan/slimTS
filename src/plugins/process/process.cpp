b8fe0a7b3dd7f24ea235945e64e8409a3c9f073c
Jeff Greer <greergan@gmail.com>
2025-04-20 15:12:38 -0700

    process object stub

--- src/plugins/process/process.cpp
+++ src/plugins/process/process.cpp
@@ -2,22 +2,28 @@
 #include <slim/gv8.h>
 #include <slim/plugin.hpp>
 #include <slim/utilities.h>
-namespace slim::process {
+namespace slim::plugin::process {
 	bool browser;
 	void exit_wrapper(const v8::FunctionCallbackInfo<v8::Value>& args);
 	void nextTick(const v8::FunctionCallbackInfo<v8::Value>& args);
+	void platform(const v8::FunctionCallbackInfo<v8::Value>& args);
 }
-void slim::process::exit_wrapper(const v8::FunctionCallbackInfo<v8::Value>& args) {
+void slim::plugin::process::exit_wrapper(const v8::FunctionCallbackInfo<v8::Value>& args) {
 	exit(1);
 }
-void slim::process::nextTick(const v8::FunctionCallbackInfo<v8::Value>& args) {
+void slim::plugin::process::nextTick(const v8::FunctionCallbackInfo<v8::Value>& args) {
 
+}
+void slim::plugin::process::platform(const v8::FunctionCallbackInfo<v8::Value>& args) {
+	v8::Isolate* isolate = args.GetIsolate();
+	args.GetReturnValue().Set(slim::utilities::StringToV8String(isolate, "linux"));
 }
 extern "C" void expose_plugin(v8::Isolate* isolate) {
 	slim::plugin::plugin process_plugin(isolate, "process");
-	process_plugin.add_property("browser", &slim::process::browser);
-	process_plugin.add_function("exit", slim::process::exit_wrapper);
-	process_plugin.add_function("nextTick", slim::process::nextTick);
+	process_plugin.add_property("browser", &slim::plugin::process::browser);
+	process_plugin.add_function("exit", slim::plugin::process::exit_wrapper);
+	process_plugin.add_function("nextTick", slim::plugin::process::nextTick);
+	process_plugin.add_function("platform", slim::plugin::process::platform);
 	process_plugin.expose_plugin();
 	return;
 }
