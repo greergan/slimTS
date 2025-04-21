01f928aa03ae4859aec983655a6f24fc572de88d
Jeff Greer <greergan@gmail.com>
2025-04-21 00:38:25 -0700

    added colors

--- src/slim_common_log.cpp
+++ src/slim_common_log.cpp
@@ -3,15 +3,27 @@
 
 static std::map<std::string, std::string> colors {
 	{"cyan", "\033[0;36m"},
+	{"blue", "\033[0;34m"},
+	{"green", "\033[0;32m"},
+	{"purple", "\033[0;35m"},
  	{"red", "\033[0;31m"},
-	{"yellow", "\033[0;33m"},
-	{"white", "\033[1;37m"}
+	{"white", "\033[0;37m"},
+	{"yellow", "\033[0;33m"}
 };
 static std::string colorize(const char* input, const char* color) {
 	return std::string(colors[const_cast<char*>(color)]) + std::string(input) + "\033[0m";
 }
+static std::string blue(const char* input) {
+	return colorize(input, "blue");
+}
 static std::string cyan(const char* input) {
-	return colorize(input, "red");
+	return colorize(input, "cyan");
+}
+static std::string green(const char* input) {
+	return colorize(input, "green");
+}
+static std::string purple(const char* input) {
+	return colorize(input, "purple");
 }
 static std::string red(const char* input) {
 	return colorize(input, "red");
@@ -28,7 +40,7 @@ void slim::common::log::debug(bool value) {
 }
 void slim::common::log::debug(const slim::common::log::Message message) {
 	if(slim::common::log::allow_debug)
-		slim::common::log::print(cyan("DEBUG => "), message);
+		slim::common::log::print(yellow("DEBUG => "), message);
 }
 void slim::common::log::error(bool value) {
 	slim::common::log::allow_error = value;
@@ -49,11 +61,11 @@ void slim::common::log::trace(bool value) {
 }
 void slim::common::log::trace(const slim::common::log::Message message) {
 	if(slim::common::log::allow_trace)
-		slim::common::log::print(yellow("TRACE => "), message);
+		slim::common::log::print(cyan("TRACE => "), message);
 }
 void slim::common::log::print(const std::string log_level, const slim::common::log::Message message) {
 	if(message.call == nullptr)
-		std::cout << log_level << std::to_string(message.line_number) << ":" << message.file << ":" << message.text << std::endl;
+		std::cout << log_level << blue(std::to_string(message.line_number).c_str()) << ":" << purple(message.file) << ":" << white(message.text) << std::endl;
 	else
-		std::cout << log_level << std::to_string(message.line_number) << ":" << message.file << ":" << message.call << ":" << message.text << std::endl;
+		std::cout << log_level << blue(std::to_string(message.line_number).c_str()) << ":" << purple(message.file) << ":" << green(message.call) << ":" << white(message.text) << std::endl;
 }
