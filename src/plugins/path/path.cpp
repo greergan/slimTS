44a1593ae34ec651ff0fd62579ddd5b0e5d2fb00
Jeff Greer <greergan@gmail.com>
2025-04-21 21:34:46 -0700

    add path object stubs and build files

---
+++ src/plugins/path/path.cpp
@@ -0,0 +1,25 @@
+#include <v8.h>
+#include <slim/gv8.h>
+#include <slim/plugin.hpp>
+#include <slim/utilities.h>
+namespace slim::plugin::path {
+	//void platform(const v8::FunctionCallbackInfo<v8::Value>& args);
+}
+/* void slim::plugin::os::platform(const v8::FunctionCallbackInfo<v8::Value>& args) {
+	v8::Isolate* isolate = args.GetIsolate();
+	args.GetReturnValue().Set(slim::utilities::StringToV8String(isolate, "linux"));
+} */
+extern "C" void expose_plugin(v8::Isolate* isolate) {
+	slim::plugin::plugin path_plugin(isolate, "path");
+	//path_plugin.add_function("platform", slim::plugin::path::platform);
+	path_plugin.expose_plugin();
+	return;
+}
+
+/*
+
+process !== "undefined" && !!process.nextTick && !process.browser
+
+https://nodejs.org/en/learn/asynchronous-work/understanding-processnexttick
+
+*/
