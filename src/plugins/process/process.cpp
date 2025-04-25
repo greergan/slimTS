42e16e524360ddbee02379146a2b6e52087abb76
Jeff Greer <greergan@gmail.com>
2025-04-24 22:45:15 -0700

    added cwd function

--- src/plugins/process/process.cpp
+++ src/plugins/process/process.cpp
@@ -1,3 +1,4 @@
+#include <filesystem>
 #include <string>
 #include <vector>
 #include <v8.h>
@@ -8,11 +9,20 @@
 #include <slim/utilities.h>
 namespace slim::plugin::process {
 	bool browser;
+	std::filesystem::path current_working_directory;
 	std::string TSC_WATCHFILE; // typescript env variable
 	v8::Local<v8::Array> args_array;
+	void cwd(const v8::FunctionCallbackInfo<v8::Value>& args);
 	void exit_wrapper(const v8::FunctionCallbackInfo<v8::Value>& args);
 	void nextTick(const v8::FunctionCallbackInfo<v8::Value>& args);
 	void platform(const v8::FunctionCallbackInfo<v8::Value>& args);
+}
+void slim::plugin::process::cwd(const v8::FunctionCallbackInfo<v8::Value>& args) {
+	v8::Isolate* isolate = args.GetIsolate();
+	if(slim::plugin::process::current_working_directory.empty()) {
+		slim::plugin::process::current_working_directory = std::filesystem::current_path();
+	}
+	args.GetReturnValue().Set(slim::utilities::StringToV8String(isolate, slim::plugin::process::current_working_directory));
 }
 void slim::plugin::process::exit_wrapper(const v8::FunctionCallbackInfo<v8::Value>& args) {
 	exit(1);
@@ -29,6 +39,7 @@ extern "C" void expose_plugin(v8::Isolate* isolate) {
 	slim::plugin::plugin process_env_plugin(isolate, "env");
 	process_env_plugin.add_property("TSC_WATCHFILE", &slim::plugin::process::TSC_WATCHFILE);
 	process_plugin.add_property("browser", &slim::plugin::process::browser);
+	process_plugin.add_function("cwd", 	slim::plugin::process::cwd);
 	process_plugin.add_function("exit", slim::plugin::process::exit_wrapper);
 	process_plugin.add_function("nextTick", slim::plugin::process::nextTick);
 	process_plugin.add_function("platform", slim::plugin::process::platform);
@@ -42,7 +53,7 @@ extern "C" void expose_plugin(v8::Isolate* isolate) {
 		argv_array->Set(context, argv_index, slim::utilities::StringToV8Value(isolate, command_line_argument));
 		argv_index++;
 	}
-	v8::Maybe<bool> set_results = full_process_plugin_object->Set(isolate->GetCurrentContext(), slim::utilities::StringToV8String(isolate, "argv"), argv_array);
+	v8::Maybe<bool> set_results = full_process_plugin_object->Set(context, slim::utilities::StringToV8String(isolate, "argv"), argv_array);
 	if(!set_results.IsJust()) {
 		isolate->ThrowException(slim::utilities::StringToV8String(isolate, "slim::plugin::process::expose_plugin() failed to load object properties"));
 	}
