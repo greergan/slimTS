d8d5ce25a9ab6967d0235298a55172f74c903966
Jeff Greer <greergan@gmail.com>
2025-04-28 00:00:32 -0700

    testing the os object

--- test/os/os.js
+++ test/os/os.js
@@ -2,6 +2,4 @@ slim.load("console")
 slim.load("os")
 console.trace("inspecting os object after creation")
 console.log(os)
-os.platform = "new_platform"
-console.trace("inspecting os object after attempted mutation of os.platform")
-console.log(os)
+console.log(os.platform())
