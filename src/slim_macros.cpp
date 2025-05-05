cd1b3c550ab91e56f8bea5d6518ace7655af9802
Jeff Greer <greergan@gmail.com>
2025-05-04 21:02:50 -0700

    simple single line transformation of require statements into import statements.

--- src/slim_macros.cpp
+++ src/slim_macros.cpp
@@ -1,4 +1,5 @@
 #include <cstring>
+#include <regex>
 #include <string>
 #include <sstream>
 #include <slim/common/log.h>
@@ -34,6 +35,9 @@ std::string slim::macros::apply(std::stringstream& input_string_stream, const st
 		replace_token("__line_number", line_of_text, std::to_string(line_number), false);
 		output_string_stream << line_of_text << std::endl;
 	}
+	std::regex require_pattern("\\s*(const|let)\\s+(\\w*)\\s*=\\s+require\\(\\s*(\"|\')(\\w+)(\"|\')\\s*\\)");
+	std::string import_statement = "\nimport $2 from '$4'";
+	std::string return_string = std::regex_replace(output_string_stream.str(), require_pattern, import_statement);
 	slim::common::log::trace(slim::common::log::Message("slim::macros::apply()","ends",__FILE__, __LINE__));
-	return output_string_stream.str();
+	return return_string;
 }
