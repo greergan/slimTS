125945a60167c9dc7a3710078927484a1d01338e
Jeff Greer <greergan@gmail.com>
2025-05-29 16:42:23 -0700

    added new library path array for bundling

--- bin/bundle_lib_files.mjs
+++ bin/bundle_lib_files.mjs
@@ -1,21 +1,41 @@
 import fs from 'fs'
 import path from 'path'
-const library_path = path.join(process.cwd(), "../lib/typescript")
+const custom_library_files = [];
+const library_paths = [path.join(process.cwd(), "../lib/typescript"), path.join(process.cwd(), "../lib/custom_definitions")];
 const cpp_file = '../src/slim_builtins_typescript_initial_library.cpp'
+const cpp_custom_libraries = '../src/slim_builtins_typescript_initial_custom_libraries.cpp'
 let output = "#include <memory>\n"
 output += "#include <string>\n"
 output += "#include <slim/common/memory_mapper.h>\n"
 output += "namespace slim::builtins::typescript {\n"
 output += "slim::common::memory_mapper::map_container typescript_map_container = {"
-const files = fs.readdirSync(library_path)
-files.map((file) => {
-	if(file.endsWith(".d.ts")) {{
-		const content = fs.readFileSync(library_path + "/" + file, 'utf8')
-		output += `\{"${file}", std::make_shared<std::string>(R"+++(${content})+++")\},`
-	}}
+library_paths.map((library_path) => {
+	const files = fs.readdirSync(library_path)
+	files.map((file) => {
+		if(file.endsWith(".d.ts")) {{
+			const content = fs.readFileSync(library_path + "/" + file, 'utf8')
+			let library_file = file;
+			if(library_path.endsWith("lib/custom_definitions")) {
+				library_file = `/lib/${file}`;
+				custom_library_files.push(library_file)
+			}
+			output += `\{"${library_file}", std::make_shared<std::string>(R"+++(${content})+++")\},`
+		}}
+	})
 })
 const content = fs.readFileSync(path.join(process.cwd(), "../lib") + "/" + "slim_typescript.mjs", 'utf8')
 output += `\{"slim_typescript.mjs", std::make_shared<std::string>(R"+++(${content})+++")\},`
-const fixed_output = output.slice(0, -1) + "};}"
+let fixed_output = output.slice(0, -1) + "};}"
 console.log(`writing ${cpp_file}`)
 fs.writeFileSync(cpp_file, fixed_output)
+
+
+const tsconfig = {
+  "compilerOptions": {
+	"paths": {
+		"*": custom_library_files
+	}
+  }
+}
+
+//console.log(tsconfig)
