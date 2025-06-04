9d937279611d6803b2de520d4ccfd8ca30ab9d18
Jeff Greer <greergan@gmail.com>
2025-06-03 18:56:53 -0700

    metrics specific code

--- include/slim/common/network/client/request/handler/metrics.h
+++ include/slim/common/network/client/request/handler/metrics.h
@@ -6,6 +6,6 @@
 namespace slim::common::network::client::request::handler::metrics {
 	static std::string get_metrics(slim::common::Metrics& metrics);
 	std::string handle_request(
-		char* data, slim::common::network::client::connection::Information& client_connection_information, slim::common::Metrics& metrics);
+		char* data, const slim::common::network::client::connection::Information& client_connection_information, slim::common::Metrics& metrics);
 }
 #endif
