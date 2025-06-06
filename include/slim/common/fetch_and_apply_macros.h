6d98d678ffbdd62f76b0eb06383fa21404231842
Jeff Greer <greergan@gmail.com>
2025-06-05 18:48:58 -0700

    removed artifacts which combined fetch and apply macros

--- include/slim/common/fetch_and_apply_macros.h
+++
@@ -1,7 +0,0 @@
-#ifndef __SLIM__COMMON_FETCH_AND_PROCESS__H
-#define __SLIM__COMMON_FETCH_AND_PROCESS__H
-#include <string>
-namespace slim::common {
-	std::string fetch_and_apply_macros(std::string& file_name_string);
-}
-#endif
