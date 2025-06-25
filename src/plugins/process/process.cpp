beb459a7406d336f3beb1f1646c5089c0eb8cfcc
Jeff Greer <greergan@gmail.com>
2025-06-24 23:07:17 -0700

    added code to translate command line arguments into the scripts process.argv array. also added pid

--- src/plugins/process/process.cpp
+++ src/plugins/process/process.cpp
@@ -1,11 +1,16 @@
+#include <sys/types.h>
+#include <unistd.h>
 #include <filesystem>
 #include <iostream>
+#include <memory>
+#include <sstream>
 #include <string>
 #include <vector>
 #include <v8.h>
 #include <slim/command_line_handler.h>
 #include <slim/common/log.h>
 #include <slim/common/platform.h>
+#include <slim/common/memory_mapper.h>
 #include <slim/plugin.hpp>
 #include <slim/utilities.h>
 namespace slim::plugin::process {
@@ -42,10 +47,13 @@ void slim::plugin::process::stdout_write(const v8::FunctionCallbackInfo<v8::Valu
 	std::cout << value;
 }
 extern "C" void expose_plugin(v8::Isolate* isolate) {
+	v8::HandleScope isolate_scope(isolate);
+	auto context = isolate->GetCurrentContext();
 	slim::plugin::plugin process_plugin(isolate, "process");
 	slim::plugin::plugin process_env_plugin(isolate, "env");
 	slim::plugin::plugin process_stderr_plugin(isolate, "stderr");
 	slim::plugin::plugin process_stdout_plugin(isolate, "stdout");
+	process_plugin.add_property_immutable("pid", getpid());
 	process_plugin.add_property_immutable("platform", slim::common::platform::platform);
 	process_env_plugin.add_property("TSC_WATCHFILE", &slim::plugin::process::TSC_WATCHFILE);
 	process_plugin.add_function("cwd", 	slim::plugin::process::cwd);
@@ -56,27 +64,16 @@ extern "C" void expose_plugin(v8::Isolate* isolate) {
 	process_plugin.add_plugin("env", &process_env_plugin);
 	process_plugin.add_plugin("stderr", &process_stderr_plugin);
 	process_plugin.add_plugin("stdout", &process_stdout_plugin);
-	v8::Local<v8::Context> context = isolate->GetCurrentContext();
-	v8::Local<v8::Object> full_process_plugin_object = process_plugin.new_instance();
-	v8::Local<v8::Array> argv_array = v8::Array::New(isolate);
-	std::vector<std::string> argv_vector;// = slim::command_line::get_argv();
-	uint32_t argv_index = 0;
-/* 	for(auto& command_line_argument: argv_vector) {
-		auto result = argv_array->Set(context, argv_index, slim::utilities::StringToV8Value(isolate, command_line_argument));
-		argv_index++;
-	} */
-	v8::Maybe<bool> set_results = full_process_plugin_object->Set(context, slim::utilities::StringToV8String(isolate, "argv"), argv_array);
-	if(!set_results.IsJust()) {
-		isolate->ThrowException(slim::utilities::StringToV8String(isolate, "slim::plugin::process::expose_plugin() failed to load object properties"));
+	auto v8_argv_array = v8::Array::New(isolate);
+	std::stringstream script_argv_stream(*slim::common::memory_mapper::read("configurations", "script.argv").get());
+	std::string arg;
+	int argc = 0;
+	while(std::getline(script_argv_stream, arg, ',')) {
+		auto result = v8_argv_array->Set(context, argc, slim::utilities::StringToV8Value(isolate, arg));
+		argc++;
 	}
-	process_plugin.expose_plugin(full_process_plugin_object);
+	auto instance_of_object = process_plugin.new_instance();
+	instance_of_object->Set(context, slim::utilities::StringToV8String(isolate, "argv"), v8_argv_array);
+	process_plugin.expose_plugin(instance_of_object);
 	return;
 }
-
-/*
-
-process !== "undefined" && !!process.nextTick && !process.browser
-
-https://nodejs.org/en/learn/asynchronous-work/understanding-processnexttick
-
-*/
